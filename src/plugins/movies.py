9b07bcf9d05b3f5c8eee7b49e219e06b8670c56a
Nikita Lötkemann <nikita.loetkemann@fh-bielefeld.de>
2020-03-12 21:34:10 +0100

    get movies of actor

--- src/plugins/movies.py
+++ src/plugins/movies.py
@@ -6,6 +6,7 @@ from imdb import IMDb
 
 class Movies(Plugin):
     max_cast_count = 7
+    max_movies_count = 10
 
     def __init__(self, name, config):
         super().__init__(name, config)
@@ -18,6 +19,12 @@ class Movies(Plugin):
         assert isinstance(movie, Movie)
         return movie
 
+    def __get_actor(self, actor_name):
+        actor = self.imdb.search_person(actor_name)
+        actor = self.imdb.get_person(actor[0].getID())
+        assert isinstance(actor, Person)
+        return actor
+
     def get_movie_description(self, args: dict):
         """
         performes a search at wiki
@@ -28,9 +35,13 @@ class Movies(Plugin):
         movie = self.__get_movie(args['$movie'])
         title = movie['title']
         year = movie['year']
-        plot = movie['plot'][0]
+        plot = ''
+        length = 0
+        for i in range(0, len(movie['plot']) if len(movie['plot']) < 3 else 3):
+            if len(movie['plot'][i]) > length:
+                plot = movie['plot'][i]
 
-        plot = plot.replace('::', '__') + '__'
+        plot = plot.replace('::', '\n- __') + '__'
 
         answer = '**{title}** ({year}) handelt von:\n{plot}'.format(title=title, year=year, plot=plot)
         return {'$description': answer}
@@ -55,3 +66,42 @@ class Movies(Plugin):
 
         answer = '**{title}** ({year}) ist besetzt mit:{cast}'.format(title=title, year=year, cast=all_cast)
         return {'$cast': answer}
+
+    def actor_details(self, args):
+        self.requiere_param(args, '$actor')
+        actor_name = args['$actor']
+
+        actor = self.__get_actor(actor_name)
+
+        filmography = actor['filmography']
+
+        movies = ''
+        for movie in filmography:
+            for key in movie:
+                counter = 0
+                if 'actor' == key or 'actress' == key:
+                    movies += '\n**Schauspieler**:'
+                    for actor_movie in movie[key]:
+                        if counter < self.max_movies_count:
+                            movies += '\n- {0}'.format(str(actor_movie))
+                            counter += 1
+                        else:
+                            break
+                elif 'producer' == key:
+                    movies += '\n**Producer**:'
+                    for producer_movie in movie[key]:
+                        if counter < self.max_movies_count:
+                            movies += '\n- {0}'.format(str(producer_movie))
+                            counter += 1
+                        else:
+                            break
+                elif 'director' == key:
+                    movies += '\n**Director**:'
+                    for director_movie in movie[key]:
+                        if counter < self.max_movies_count:
+                            movies += '\n- {0}'.format(str(director_movie))
+                            counter += 1
+                        else:
+                            break
+
+        return {'$details': movies}
